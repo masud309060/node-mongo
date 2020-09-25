8a1984edfdbbc1f8762d90852adbcc43cc6c9d4c
masud309060 <masud306090@gmail.com>
2020-09-26 01:32:39 +0600

    my first node projects

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+
+const app = express()
+
+app.get('/', (req, res) => {res.send("I know how to run node .. Yay")})
+
+app.listen(4200, ()=> console.log("listening to port 4200"))
